be60bb28f7843ffe71fc2c4b7033d070642eb1f4
Andrew Faircloth <andrewkfaircloth@gmail.com>
2015-11-22 23:13:02 -0500

    finished form errors and styling of contacts

--- app/js/main.js
+++ app/js/main.js
@@ -55,13 +55,31 @@ var AddController = function AddController(ContactService, $state, $scope) {
   var vm = this;
 
   vm.addContact = addContact;
+  vm.validateEmail = validateEmail;
+  vm.validateEmpty = validateEmpty;
+  vm.validateWebsite = validateWebsite;
 
   $scope.$watch('contact.email', function (newVal) {
 
     if (!newVal) return;
 
     if (!validateEmail(newVal)) {
-      return console.log('email needs an @symbol');
+      $scope.contact.emailError = 'Email needs an @ symbol';
+      return console.log('email needs an @ symbol');
+    } else {
+      $scope.contact.emailError = undefined;
+    }
+  });
+
+  $scope.$watch('contact.website', function (newVal) {
+
+    if (!newVal) return;
+
+    if (!validateWebsite(newVal)) {
+      $scope.contact.webError = 'Website must contain http(s)';
+      return console.log('Website must contain http(s)');
+    } else {
+      $scope.contact.webError = undefined;
     }
   });
 
@@ -69,10 +87,16 @@ var AddController = function AddController(ContactService, $state, $scope) {
 
     if (!validateEmpty(contactObj.firstName)) {
       console.log('first name is empty');
+      $scope.contact.fnError = "First name cannot be empty";
     }
 
     if (!validateEmpty(contactObj.lastName)) {
       console.log('last name is empty');
+      $scope.contact.lnError = "Last name cannot be empty";
+    }
+
+    if (!validateEmpty(contactObj.email)) {
+      console.log('email is empty');
     }
 
     if (!validateEmpty(contactObj.email)) {
@@ -87,12 +111,17 @@ var AddController = function AddController(ContactService, $state, $scope) {
       return console.log('website needs an http(s)');
     }
 
+    if (!validateEmpty(contactObj.comment)) {
+      console.log('email is empty');
+      $scope.contact.commentError = "Comment cannot be empty";
+    }
+
     console.log('added to the server');
 
-    // ContactService.addContact(contactObj).then( (res) =>{
-    //   console.log(res);
-    //   $state.go('root.home');
-    // });
+    ContactService.addContact(contactObj).then(function (res) {
+      console.log(res);
+      $state.go('root.home');
+    });
   }
 
   function validateEmpty(field) {
--- js/controllers/add.controller.js
+++ js/controllers/add.controller.js
@@ -3,24 +3,51 @@ let AddController = function(ContactService, $state, $scope) {
   let vm = this;
 
   vm.addContact = addContact;
+  vm.validateEmail = validateEmail;
+  vm.validateEmpty = validateEmpty;
+  vm.validateWebsite = validateWebsite;
   
   $scope.$watch('contact.email', function (newVal) {
     
-    if (!newVal) return;
+    if(!newVal) return;
 
-    if (!validateEmail(newVal)) {
-      return console.log('email needs an @symbol');
+    if(!validateEmail(newVal)) {
+      $scope.contact.emailError = 'Email needs an @ symbol';
+      return console.log('email needs an @ symbol');
+    } else {
+      $scope.contact.emailError = undefined;
     }
+
+  });
+
+  $scope.$watch('contact.website', function (newVal) {
+    
+    if(!newVal) return;
+
+    if(!validateWebsite(newVal)) {
+      $scope.contact.webError = 'Website must contain http(s)';
+      return console.log('Website must contain http(s)');
+    } else {
+      $scope.contact.webError = undefined;
+    }
+
   });
 
   function addContact (contactObj) {
     
     if (!validateEmpty(contactObj.firstName)) {
       console.log('first name is empty');
+      $scope.contact.fnError = "First name cannot be empty";
     }
 
     if (!validateEmpty(contactObj.lastName)) {
       console.log('last name is empty');
+      $scope.contact.lnError = "Last name cannot be empty";
+    }
+
+    if (!validateEmpty(contactObj.email)) {
+      console.log('email is empty');
+
     }
 
     if (!validateEmpty(contactObj.email)) {
@@ -31,17 +58,21 @@ let AddController = function(ContactService, $state, $scope) {
       console.log('website is empty');
     }
 
-
     if (!validateWebsite(contactObj.website)) {
       return console.log('website needs an http(s)');
     }
 
+    if (!validateEmpty(contactObj.comment)) {
+      console.log('email is empty');
+      $scope.contact.commentError = "Comment cannot be empty";
+    }
+
     console.log('added to the server');
 
-    // ContactService.addContact(contactObj).then( (res) =>{
-    //   console.log(res);
-    //   $state.go('root.home');
-    // });
+    ContactService.addContact(contactObj).then( (res) =>{
+      console.log(res);
+      $state.go('root.home');
+    });
   }
 
   function validateEmpty (field) {
